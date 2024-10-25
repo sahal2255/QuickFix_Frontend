75a6c6075075841be1f588f67348c67a14f4d5ff
sahal2255 <sahalvv9656@gmail.com>
2024-10-25 19:23:28 +0530

    pslu

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Default to 3000 if PORT is not set
+    host: true, // Enables network access
+  },
+});
