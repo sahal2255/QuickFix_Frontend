ea552274880866d0d97c754fec9b05eb6d35d93b
sahal2255 <sahalvv9656@gmail.com>
2024-11-15 10:38:50 +0530

    foo

--- src/components/layouts/user/ConfirmBooking.jsx
+++ src/components/layouts/user/ConfirmBooking.jsx
@@ -65,103 +65,105 @@ export default function ConfirmBooking() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <header className="fixed top-0 left-0 right-0 z-10 bg-white shadow-lg">
         <Navbar />
       </header>
 
-      <div className="flex flex-col mt-4 pt-24 md:flex-row md:space-x-8 max-w-7xl mx-auto px-4 md:px-8">
-        <div className="w-full md:w-1/3">
-          <ServiceTypeSidebar serviceId={serviceId} serviceList={serviceList} />
-        </div>
+      <main className="flex-grow pt-24">
+        <div className="flex flex-col mt-4 md:flex-row md:space-x-8 max-w-7xl mx-auto px-4 md:px-8">
+          <div className="w-full md:w-1/3">
+            <ServiceTypeSidebar serviceId={serviceId} serviceList={serviceList} />
+          </div>
 
-        <div className="w-full md:w-2/3 bg-white shadow-lg p-6 rounded-md">
-          <h2 className="text-3xl font-bold mb-4">Service Details</h2>
+          <div className="w-full md:w-2/3 bg-white shadow-lg p-6 rounded-md">
+            <h2 className="text-3xl font-bold mb-4">Service Details</h2>
 
-          {selectedServiceTypesDetails.length > 0 ? (
-            <div className="overflow-x-auto">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Service Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Price
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {selectedServiceTypesDetails.map((serviceType) => (
-                    <tr key={serviceType._id}>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {serviceType.serviceName}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        ₹{serviceType.price}
-                      </td>
+            {selectedServiceTypesDetails.length > 0 ? (
+              <div className="overflow-x-auto">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Service Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Price
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {selectedServiceTypesDetails.map((serviceType) => (
+                      <tr key={serviceType._id}>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                          {serviceType.serviceName}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          ₹{serviceType.price}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
 
-              {/* Total Price Section */}
-              <div className="mt-6">
-                <h3 className="text-xl font-semibold text-gray-700">
-                  Total Price: <span className="text-green-600"> ₹{totalPrice}</span>
-                </h3>
-              </div>
-
-              <PaymentOption onSelectPayment={(option) => setPaymentOption(option)} />
-
-              <div className="mt-6">
-                <h3 className="text-lg font-medium text-gray-700">
-                  {paymentOption === 'advance'
-                    ? `Advance Payment (50%): ₹${paymentAmount}`
-                    : `Full Payment: ₹${paymentAmount}`}
-                </h3>
-              </div>
-
-              {/* Show coupons only if Full Payment is selected */}
-              {paymentOption === 'full' && (
-                <div className="mt-6 flex flex-wrap gap-2"> {/* Use flex and wrap */}
-                  {validCoupons.length > 0 ? (
-                    validCoupons.map((coupon) => (
-                      <Coupon
-                        key={coupon._id}
-                        couponCode={coupon.couponName}
-                        discount={coupon.couponValue}
-                        expiryDate={new Date(coupon.endDate).toLocaleDateString()}
-                        selected={selectedCoupon && selectedCoupon._id === coupon._id} // Check if the coupon is selected
-                        onSelect={() => setSelectedCoupon(coupon)} // Set the entire coupon object as selected
-                      />
-                    ))
-                  ) : (
-                    <p className="text-gray-600">No valid coupons available.</p>
-                  )}
+                {/* Total Price Section */}
+                <div className="mt-6">
+                  <h3 className="text-xl font-semibold text-gray-700">
+                    Total Price: <span className="text-green-600"> ₹{totalPrice}</span>
+                  </h3>
                 </div>
-              )}
 
-              <div className="mt-6 flex justify-center">
-                <button
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md"
-                  onClick={handleSubmit}
-                >
-                  Book Now
-                </button>
+                <PaymentOption onSelectPayment={(option) => setPaymentOption(option)} />
+
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-gray-700">
+                    {paymentOption === 'advance'
+                      ? `Advance Payment (50%): ₹${paymentAmount}`
+                      : `Full Payment: ₹${paymentAmount}`}
+                  </h3>
+                </div>
+
+                {/* Show coupons only if Full Payment is selected */}
+                {paymentOption === 'full' && (
+                  <div className="mt-6 flex flex-wrap gap-2">
+                    {validCoupons.length > 0 ? (
+                      validCoupons.map((coupon) => (
+                        <Coupon
+                          key={coupon._id}
+                          couponCode={coupon.couponName}
+                          discount={coupon.couponValue}
+                          expiryDate={new Date(coupon.endDate).toLocaleDateString()}
+                          selected={selectedCoupon && selectedCoupon._id === coupon._id}
+                          onSelect={() => setSelectedCoupon(coupon)}
+                        />
+                      ))
+                    ) : (
+                      <p className="text-gray-600">No valid coupons available.</p>
+                    )}
+                  </div>
+                )}
+
+                <div className="mt-6 flex justify-center">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md"
+                    onClick={handleSubmit}
+                  >
+                    Book Now
+                  </button>
+                </div>
               </div>
-            </div>
-          ) : (
-            <p className="text-red-600">No selected service types.</p>
-          )}
+            ) : (
+              <p className="text-red-600">No selected service types.</p>
+            )}
+          </div>
         </div>
-      </div>
+      </main>
 
       {/* Modal to show ConfirmForm */}
       <CommonModal open={openModalForm} onCancel={closeConfirmForm}>
@@ -175,9 +177,11 @@ export default function ConfirmBooking() {
           closeConfirmForm={closeConfirmForm}
         />
       </CommonModal>
-      <div className="w-full mt-6">
+
+      {/* Footer */}
+      <footer className="w-full bg-gray-100">
         <Footer />
-      </div>
+      </footer>
     </div>
   );
 }
